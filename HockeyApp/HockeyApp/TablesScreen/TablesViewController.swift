ed668756650e82bfb1fabd35638ac4cf92faaeff
Vlad <vlad_sklyarov@mail.ru>
2021-11-20 20:47:29 +0700

    Создан проект

---
+++ HockeyApp/HockeyApp/TablesScreen/TablesViewController.swift
@@ -0,0 +1,10 @@
+//
+//  TablesViewController.swift
+//  HockeyApp
+//
+//  Created by Vlad on 20.11.2021.
+//
+
+import Foundation
+import UIKit
+
---
+++ HockeyApp/Pods/Target Support Files/Pods-HockeyApp/Pods-HockeyApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HockeyApp : NSObject
+@end
+@implementation PodsDummy_Pods_HockeyApp
+@end
